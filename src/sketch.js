7246ff241456ac6a4734fd5de08230fb049ac19d
Pedro Mendonca <peumendonca@gmail.com>
2017-04-22 13:56:42 +0100

    Added ts functionality

---
+++ src/sketch.js
@@ -0,0 +1,11 @@
+"use strict";
+exports.__esModule = true;
+require("../lib/p5.js");
+function setup() {
+    createCanvas(800, 600);
+    background(155);
+}
+function draw() {
+    rect(100, 100, 10, 10);
+    rect(100, 100, 10, 10);
+}
--- src/sketch.ts
+++ src/sketch.ts
@@ -1,6 +1,9 @@
+import "../lib/p5.global-mode"
 function setup(){
-
+	createCanvas(800,600)
+	background(155)
 }
 function draw(){
-
+	rect(100,100,10,10)
+	rect(100,100,10,10)
 }
