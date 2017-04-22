768bcd356cedee524de55f32f0e4968aa64390f9
Jonathan Balls <j.balls@btopenworld.com>
2017-04-22 15:18:33 +0200

    Server starts

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,27 @@
 #!/usr/bin/env/ node
 
-import 'express';
-import 'http';
-import 'socket.io';
+import * as express from 'express';
+import * as http from 'http';
+import * as io from 'socket.io';
+
+var verbose = false;
+var gameport = 4242;
+var app = express();
+var server = http.createServer(app);
 
 console.log("Vikings of Christiana running..");
+
+server.listen(gameport);
+console.log(':: Listening on port ' + gameport);
+
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/game.html');
+});
+app.get('/static/*', function(req, res, next) {
+    var file = req.params[0];
+
+    if(verbose) console.log('\t :: Express :: file requested : ' + file);
+
+    res.sendFile(__dirname + '/static/' + file);
+});
