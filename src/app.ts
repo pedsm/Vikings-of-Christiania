da97c5c693c8073056d037b2b21b4968a402e08c
Jonathan Balls <j.balls@btopenworld.com>
2017-04-22 17:23:01 +0200

    Create player

--- src/app.ts
+++ src/app.ts
@@ -3,17 +3,20 @@
 import * as express from 'express';
 import * as http from 'http';
 import * as io from 'socket.io';
+import {Player, Projectile} from './player';
 
 var verbose = false;
 var gameport = 4242;
 var app = express();
 var server = http.createServer(app);
+let players: Array<Player> = [];
 
 console.log("Vikings of Christiana running..");
 
 server.listen(gameport);
 console.log(':: Listening on port ' + gameport);
 
+// Serve the root file
 app.get('/', function(req, res) {
     res.sendFile('game.html', { root: __dirname + "/../"});
 });
@@ -22,3 +25,15 @@ app.get('/', function(req, res) {
 app.use(express.static('lib'))
 app.use(express.static('comp'))
 app.use(express.static('static'))
+
+// Handle a socket connection
+io().on('connection', function(socket) {
+    // Join the main server;
+    players.push()
+    socket.join("default_room");
+
+    socket.on('join_game', function(id, message) {
+        var player: Player = new Player(socket.id, message.name);
+        players.push(player);
+    })
+})
--- src/player.ts
+++ src/player.ts
@@ -1,26 +1,32 @@
 
-
 export class Player {
     name: string;
-    x: float;
-    y: float;
-    direction: float;
-    speed: float;
-    constructor (playerName: string) {
+    id: string;
+    x: number;
+    y: number;
+    direction: number;
+    constructor (id: string, playerName: string) {
+        this.id = id;
         this.name = playerName;
+
+        // TODO: make this more intelligent
+        // Sets the location between +-10;
+        this.x = 20*Math.random() - 10;
+        this.y = 20*Math.random() - 10;
+        this.direction = 0;
     }
 }
 
 export class Projectile {
     source: Player
-    x: float;
-    y: float;
-    direction: float;
-    speed: float;
+    x: number;
+    y: number;
+    direction: number;
+    speed: number;
     contructor (sourcePlayer: Player) {
         this.source = sourcePlayer;    
     }
-    updateProjectile(time: float) {
+    updateProjectile(time: number) {
         this.x += this.speed * Math.cos(this.direction) * time;
         this.y += this.speed * Math.sin(this.direction) * time;
     }
