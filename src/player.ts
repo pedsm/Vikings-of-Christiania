1d6624f5fa739a63cd5f5224ac89c91b9e0cfdac
Siokhan <siokhank@gmail.com>
2017-04-22 17:09:46 +0200

    Player and projectile class with update function

---
+++ src/player.ts
@@ -0,0 +1,26 @@
+class Player {
+    name: string;
+    x: float;
+    y: float;
+    direction: float;
+    speed: float;
+    constructor (playerName: string) {
+        this.name = playerName;
+    }
+}
+
+class Projectile {
+    source: Player
+    x: float;
+    y: float;
+    direction: float;
+    speed: float;
+    contructor (sourcePlayer: Player) {
+        this.source = sourcePlayer;    
+    }
+    updateProjectile(time: float) {
+        this.x += this.speed * Math.cos(this.direction) * time;
+        this.y += this.speed * Math.sin(this.direction) * time;
+    }
+}
+
