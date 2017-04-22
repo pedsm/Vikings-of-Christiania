fb7fe2c833c09180652f5d3b59a1a2042fa717ca
Pedro Mendonca <peumendonca@gmail.com>
2017-04-22 17:27:11 +0100

    Added camera

--- src/sketch.ts
+++ src/sketch.ts
@@ -23,15 +23,18 @@ var boats = [];
 
 function setup(){
 	createCanvas(800,600)
-	background(155)
 	boats.push(new Boat("Pedro",100,100))
 	boats.push(new Boat("Tal",0,0))
+	boats.push(new Boat("Tal",100,0))
 }
 function draw(){
-	//Camera
-	translate(width/2 + boats[0].x-boats[0].size,height/2 + boats[0].y - boats[0].size)
+	background(155)
+	push()
+	translate(width/2-boats[0].x,height/2-boats[0].y)
 	//Print all boats
 	boats.map((boat)=>{
 		boat.draw();
 	})
+	pop()
+	boats[0].y +=1
 }
