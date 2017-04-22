c8f05ef1e1196d83e2c4d5cb512b46987e3c3391
Pedro Mendonca <peumendonca@gmail.com>
2017-04-22 14:12:32 +0100

    Removed compiled.js

--- src/sketch.ts
+++ src/sketch.ts
@@ -1,9 +1,20 @@
 import "../lib/p5.global-mode"
+class Boat {
+	name:String;
+	speedX:Number;
+	speedY:Number;
+	cosntructor(input:string){
+		this.name = input
+	}
+}
+
 function setup(){
-	createCanvas(800,600)
+	createCanvas(800,600,WEBGL)
 	background(155)
 }
 function draw(){
-	rect(100,100,10,10)
-	rect(100,100,10,10)
+	rotateX(0.785398)
+	rotateY(0.785398)
+	// fill(100)
+	box(100,100,100)
 }
