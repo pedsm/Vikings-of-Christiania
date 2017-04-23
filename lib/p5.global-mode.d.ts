6c51e979371ead729a7660ac9d54a6306901386a
Pedro Mendonca <peumendonca@gmail.com>
2017-04-23 06:26:12 +0100

    Scale fix

--- lib/p5.global-mode.d.ts
+++ lib/p5.global-mode.d.ts
@@ -565,7 +565,7 @@ declare function rotate(angle: number): p5;
 //
 //   param "s" has invalid type: Number | p5.Vector | Array
 //
-// declare function scale(s: Number | p5.Vector | Array, y?: number, z?: number): p5;
+declare function scale(s: Number ): p5;
 
 /**
  * Shears a shape around the x-axis the amount specified by the angle
